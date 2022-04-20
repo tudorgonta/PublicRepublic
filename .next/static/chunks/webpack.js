4edbeda90a2b9954c29b56769315c98b1b6d4942
tudorgonta <gontatudor@gmail.com>
2022-04-20 11:57:40 +0200

    updated carousel
    object fit css update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3fc6984cb3ee74f5"; }
+/******/ 		__webpack_require__.h = function() { return "f9ab2659f6ec7bbd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

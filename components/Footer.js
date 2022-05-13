4f13133c1db02f9c49826014a583e02e0ca98f61
tudorgonta <gontatudor@gmail.com>
2022-05-14 00:27:25 +0200

    Updated category slug

--- components/Footer.js
+++ components/Footer.js
@@ -2,28 +2,28 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer class="p-4 bg-white lg shadow md:px-6 md:py-8 dark:bg-neutral-800">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="/" class="flex items-center mb-4 sm:mb-0">
-                <span class="self-center text-2xl font-Monsieur whitespace-nowrap dark:text-white">PublicRepublic</span>
+    <footer className="p-4 bg-white lg shadow md:px-6 md:py-8 dark:bg-neutral-800">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="/" className="flex items-center mb-4 sm:mb-0">
+                <span className="self-center text-2xl font-Monsieur whitespace-nowrap dark:text-white">PublicRepublic</span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Gallery</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">Gallery</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline">Contact</a>
+                    <a href="#" className="hover:underline">Contact</a>
                 </li>
             </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">PublicRepublic™</a>. All Rights Reserved.
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" className="hover:underline">PublicRepublic™</a>. All Rights Reserved.
         </span>
     </footer>
   )
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -3,32 +3,32 @@ import client from '../../client'
 import Head from 'next/head'
 
 
-const Category = ({post}) => {
-  const {
-    title = 'Missing title'
-  } = post
+const Category = ({category, slug}) => {
   return (
     <>
       <Head>
-        <title>PR - {title}</title>
+        <title>PR - {slug}</title>
       </Head>
       <article>
-        <h1>{title}</h1>
+      {category.map(({title}, index) => (
+        <h1 key={index}>{title}</h1>
+      ))}
       </article>
     </>
   )
 }
 
-const query = groq`*[_type == "post" && $slug in categories[]->slug.current]{
+const query = groq`*[_type == "post" && $slug in categories[]->title]{
   title,
   "postImage": mainImage
 }`
 export async function getServerSideProps(context) {
   const { slug = "" } = context.params
-  const post = await client.fetch(query, { slug })
+  const category = await client.fetch(query, { slug })
   return {
     props: {
-      post
+        category,
+        slug
     }
   }
 }

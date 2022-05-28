2b1cd849b1aed10919f3de9b7c868a7bbbb36cdb
tudorgonta <gontatudor@gmail.com>
2022-05-28 22:40:12 +0200

    Fancybox
    fixed fancybox components

--- components/Fancybox.js
+++ components/Fancybox.js
@@ -3,7 +3,8 @@ import React, { useEffect } from "react";
 import { Fancybox as NativeFancybox } from "@fancyapps/ui/dist/fancybox.esm.js";
 import "@fancyapps/ui/dist/fancybox.css";
 
-function Fancybox(props) {
+const Fancybox = (props) => {
+
   const delegate = props.delegate || "[data-fancybox]";
 
   useEffect(() => {

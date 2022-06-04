0fd6d6e19c60fe399c4a7224326ce40dad81cc5a
tudorgonta <gontatudor@gmail.com>
2022-06-05 01:30:52 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     container: {
@@ -20,6 +19,5 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin'),
   ], 
 }

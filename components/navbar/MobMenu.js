33f5da2ee8aca35bffa9ccffcabe9237b8029c6e
tudorgonta <gontatudor@gmail.com>
2022-06-09 21:22:27 +0200

    Update MobMenu.js

--- components/navbar/MobMenu.js
+++ components/navbar/MobMenu.js
@@ -28,7 +28,7 @@ const MenuItems = ({ sectionTitle, target, links }) => {
         aria-haspopup="menu"
         aria-expanded={dropdown ? "true" : "false"}
         onClick={() => setDropdown((prev) => !prev)}
-        className="inline-flex items-center"
+        className="inline-flex items-center uppercase"
      >
       {sectionTitle}<svg className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
      </button>

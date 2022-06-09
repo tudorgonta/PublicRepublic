64c556cb6313e9d9cb84c44107feb09c14d8f349
tudorgonta <gontatudor@gmail.com>
2022-06-09 21:33:29 +0200

    Updated App.js

--- pages/_app.js
+++ pages/_app.js
@@ -29,8 +29,8 @@ function MyApp({ Component, pageProps }) {
       <link rel="shortcut icon" href="/static/pr.png" />
     </Head> 
       <div className="max-w-screen-xl m-auto">
-      <a href="/"><h1 className="text-8xl md:text-9xl font-Romantica text-center my-10 opacity-40">PublicRepublic</h1></a>
-      <h2 className='text-sm md:text-1xl text-center uppercase -mt-14 mb-10 font-Bulkey tracking-[0.5em]'>Video &amp; Photograpghy</h2>
+      <a href="/"><h1 className="text-8xl md:text-9xl font-Romantica text-center my-1 opacity-40">PublicRepublic</h1></a>
+      <h2 className='text-sm md:text-1xl text-center uppercase -mt-5 mb-5 font-Bulkey tracking-[0.5em]'>Video &amp; Photograpghy</h2>
       <Component {...pageProps} />
     </div>
     <Footer />
